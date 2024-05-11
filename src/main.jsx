602a11eb9f0e9cab907b93d4f4a791ef631ccc8e
karam mustafa <karam2mustafa@gmail.com>
2024-05-12 02:08:22 +0300

    wip

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FirstComponent from './FirstComponent.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FirstComponent />
+  </React.StrictMode>
+)
